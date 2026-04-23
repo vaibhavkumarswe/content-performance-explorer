f748eaa178b13b21e2ba875efbe637fbf5ef5fde
Vaibhav Kumar <vaibhavkumarswe@gmail.com>
2026-04-24 02:01:52 +0530

    Init : Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <Typography variant="h1" align="center" mt={4}>
+        Hello, World!
+      </Typography>
+    </Box>
+  );
+}
+
+export default App;
